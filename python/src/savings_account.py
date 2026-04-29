ac1a881c24242bf747b8e8fabc4b93ec971031e9
ShockaHolmes <sholmes7214@gmail.com>
2026-04-29 10:18:34 -0400

    Updated person, investment, and savings account

--- python/src/savings_account.py
+++ python/src/savings_account.py
@@ -4,22 +4,17 @@ from src.account import Account
 class SavingsAccount(Account):
     def __init__(self, account_holder, balance, account_number, interest_rate):
         super().__init__(account_holder, balance, account_number)
-        # TODO: Implement constructor
+        self.interest_rate = interest_rate
 
     def get_interest_rate(self):
-        # TODO: Implement getter
-        return None
+        return self.interest_rate
 
     def set_interest_rate(self, interest_rate):
-        # TODO: Implement setter
-        pass
+        self.interest_rate = interest_rate
 
     def apply_interest(self):
-        # TODO: Implement method to apply interest to the balance
-        # New balance = current balance + (current balance * interest rate)
-        pass
+        self.balance += self.balance * self.interest_rate
 
     def debit(self, amount):
-        # TODO: Implement debit method
-        # Savings accounts have overdraft protection - don't allow balance to go negative
-        pass
+        if self.balance - amount >= 0:
+            self.balance -= amount
